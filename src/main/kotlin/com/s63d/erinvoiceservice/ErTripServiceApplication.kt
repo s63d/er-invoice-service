e3ef655e754dffb2329fb53002dcee7e7efeba27
Marvin <marvin.zwolsman@gmail.com>
2018-05-29 10:07:25 +0200

    Initial commit

---
+++ src/main/kotlin/com/s63d/erinvoiceservice/ErTripServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.s63d.ertripservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+
+@SpringBootApplication
+@EnableFeignClients
+class ErInvoiceServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ErInvoiceServiceApplication>(*args)
+}
---
+++ src/main/kotlin/com/s63d/erinvoiceservice/services/TripService.kt
@@ -0,0 +1,26 @@
+// package com.s63d.ertripservice.services
+
+// import com.s63d.ertripservice.clients.VehicleClient
+// import com.s63d.ertripservice.domain.CarTracker
+// import com.s63d.ertripservice.domain.Trip
+// import com.s63d.ertripservice.domain.TripResponse
+// import com.s63d.ertripservice.repositories.TripRepository
+// import org.springframework.stereotype.Service
+
+// @Service
+// class TripService(private val tripRepository: TripRepository, private val vehicleClient: VehicleClient) {
+
+//     fun createNew(carTrackerId: String) : Trip {
+//         val trip = Trip(CarTracker(carTrackerId))
+//         return tripRepository.save(trip)
+//     }
+
+//     fun byCarTracker(trackerId: String) = tripRepository.findByCarTrackerId(trackerId).map { TripResponse(it) }
+
+//     fun byId(id: Long) = TripResponse(tripRepository.findById(id).orElseThrow { Exception("Invalid trip id") }) // TODO response status
+
+//     fun byCarId(id: String): List<TripResponse> {
+//         val carTracker = vehicleClient.getById(id)?.carTracker ?: throw Exception("Vehicle $id not found") // TODO response status not found
+//         return byCarTracker(carTracker.carTrackerId)
+//     }
+// }
